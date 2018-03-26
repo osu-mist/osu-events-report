84248c87ec6b511f0ebdecb72322eb8d47007c49
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-03-26 10:23:44 -0700

    Print events number by department

--- osu-events-report.py
+++ osu-events-report.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import sys
+from collections import defaultdict, OrderedDict
 from utils import parse_arguments
 
 
@@ -9,9 +11,51 @@ EVENTS_URL = API_BASE_URL + 'events'
 DEPARTMENTS_URL = API_BASE_URL + 'departments'
 
 
+def send_request(url, params):
+    res = requests.get(url, params=params)
+    if res.status_code != 200:
+        sys.exit('HTTP status code: {}'.format(res.status_code))
+    else:
+        return res.json()
+
+
+def get_events():
+    params = {
+        'start': args.start,
+        'days': args.days,
+        'pp': 100,
+        'page': 1
+    }
+    res = send_request(EVENTS_URL, params)
+    events = res['events']
+    while res['page']['current'] < res['page']['total']:
+        params['page'] += 1
+        res = send_request(EVENTS_URL, params)
+        events += res['events']
+
+    return events
+
+
+def create_events_report(events):
+    total_events = len(events)
+    events_by_department = defaultdict(int)
+
+    for event in events:
+        if 'departments' in event['event']:
+            events_by_department[event['event']['departments'][0]['name']] += 1
+        else:
+            events_by_department['Uncatagorized'] += 1
+
+    events_by_department = OrderedDict(sorted(events_by_department.items()))
+
+    for department, events in events_by_department.items():
+        print('{}: {}'.format(department, events))
+    print('Total: {}'.format(total_events))
+
+
 def main():
-    res = requests.get(EVENTS_URL, params={'start': args.start, 'days': args.days})
-    print(json.dumps(res.json(), indent=4))
+    events = get_events()
+    create_events_report(events)
 
 
 if __name__ == '__main__':
